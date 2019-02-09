ad5ccbd7e1c4013afc19e5c5f704e784022cb74d
Roger Q <roger@wirelessprv-10-194-174-195.near.illinois.edu>
2019-02-09 15:03:18 -0600

    add simulation sketch

---
+++ simulation/arena_config.py
@@ -0,0 +1,6 @@
+# all parameters are measured in cm
+robot_width = 50
+robot_length = 80
+robot_height = 50
+
+scale_factor = 1 #todo
---
+++ simulation/flat_arena.py
@@ -0,0 +1,6 @@
+import arena_config
+import numpy as np
+
+class arena:
+    def __init__(self):
+        pass
